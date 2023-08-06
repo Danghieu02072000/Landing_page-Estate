90a1500e0b46bd61cdc7ece034597daa9f1923ba
Dang Hieu <danghieutieumai@gmail.com>
2023-08-06 14:00:12 +0700

    responsive project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+            "./src/js/main.js"
+],
   theme: {
     extend: {
       fontFamily : {
@@ -14,6 +16,23 @@ module.exports = {
         'gray3': '#333333',
         'orangef9': '#FF9900',
         'orangef8': '#F85A47',
+      },
+      screens: {
+        '2sm': '450px',
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       }
     },
   },
